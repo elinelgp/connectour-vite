bfbba389ec6aa0eb6a174a50929ab519089e6e10
COMMUN\PBLD02231 <ext.baptiste.longepee-dupas@sncf.fr>
2025-11-17 21:24:46 +0100

    feat: added prettier + esling + task with git cliff + husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/",
+  plugins: [react(), eslint()],
+  preview: {
+   port: 8080,
+   strictPort: true,
+  },
+  server: {
+   port: 8080,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:8080",
+  },
+ });
